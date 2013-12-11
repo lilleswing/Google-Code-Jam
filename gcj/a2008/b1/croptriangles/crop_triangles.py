08db9c6a8772249f4dd5f17a384ba9aa14a3d53d
Karl Leswing <karl.leswing@schrodinger.com>
2013-12-11 10:02:05 -0500

    Minor cleanup

--- gcj/a2008/b1/croptriangles/crop_triangles.py
+++ gcj/a2008/b1/croptriangles/crop_triangles.py
@@ -1,4 +1,25 @@
+class Case():
+    def __init__(self):
+        self.i = 0
+
+
+def parse_input(filename):
+    data = [x.strip() for x in open(filename, 'r').readlines()]
+
+
+def solve_case(case):
+    return 1
 
 
 if __name__ == "__main__":
-    print "crop trianlges"
+    import sys
+    cases = parse_input(sys.argv[1])
+    outfile_name = sys.argv[1].split('.')[0] + ".out"
+    fout = open(outfile_name, 'w')
+    case_num = 1
+    for case in cases:
+        result = solve_case(case)
+        message = "Case %d: %s" % (case_num, result)
+        fout.write("%s\n" % message)
+        print message
+        case_num += 1
--- gcj/util/maths.py
+++ gcj/util/maths.py
@@ -34,16 +34,17 @@ def brent(N):
 def sieve(n):
     """Return all primes <= n."""
     np1 = n + 1
-    s = list(range(np1))  # leave off `list()` in Python 2
+    s = range(np1)
     s[1] = 0
     sqrtn = int(round(n ** 0.5))
-    for i in range(2, sqrtn + 1):  # use `xrange()` in Python 2
+    for i in range(2, sqrtn + 1):
         if s[i]:
             s[i * i: np1: i] = [0] * len(xrange(i * i, np1, i))
     return filter(None, s)
 
 
 def prime_factor(n):
+    """Returns a set of prime factors for N using trial division"""
     k = 2
     factors = set()
     while n != 1:
