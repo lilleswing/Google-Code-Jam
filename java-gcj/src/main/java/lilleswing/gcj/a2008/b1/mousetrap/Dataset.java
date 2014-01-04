598f526c8b37db97fd105430280076ea76c8fce0
Karl Leswing <lilleswing@gmail.com>
2014-01-03 20:14:53 -0500

    lol

--- java-gcj/src/main/java/lilleswing/gcj/a2008/b1/mousetrap/Dataset.java
+++ java-gcj/src/main/java/lilleswing/gcj/a2008/b1/mousetrap/Dataset.java
@@ -2,11 +2,11 @@ package lilleswing.gcj.a2008.b1.mousetrap;
 
 import java.util.List;
 
-public class Case {
+class Dataset {
     public long deckSize;
     public List<Long> indices;
 
-    public Case(long deckSize, List<Long> indices) {
+    public Dataset(long deckSize, List<Long> indices) {
         this.deckSize = deckSize;
         this.indices = indices;
     }
--- java-gcj/src/main/java/lilleswing/gcj/a2008/b1/mousetrap/MouseTrap.java
+++ java-gcj/src/main/java/lilleswing/gcj/a2008/b1/mousetrap/MouseTrap.java
@@ -7,19 +7,18 @@ import java.util.List;
 /**
  * TODO(Leswing)
  */
-public class MouseTrap extends Problem<Case> {
+public class MouseTrap extends Problem<Dataset> {
     @Override
-    public String solve(Case aCase) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public String solve(Dataset dataset) {
+        return null;
     }
 
     @Override
-    public List<Case> parse(List<String> data) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public List<Dataset> parse(List<String> data) {
+        return null;
     }
 
     @Override
     public void precompute() {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
