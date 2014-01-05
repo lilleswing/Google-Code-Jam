550fe7bb36d9f1a4ed6120077b7217b3f140c33a
Karl Leswing <lilleswing@gmail.com>
2014-01-05 16:29:00 -0500

    boilerplate for fairsquare

---
+++ java-gcj/src/main/java/lilleswing/gcj/a2013/qualification/fairsquare/FairSquare.java
@@ -0,0 +1,25 @@
+package lilleswing.gcj.a2013.qualification.fairsquare;
+
+import lilleswing.gcj.util.Problem;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class FairSquare extends Problem<Dataset> {
+
+    List<BigInteger> sieve;
+
+    @Override
+    public String solve(Dataset dataset) {
+        return null;
+    }
+
+    @Override
+    public List<Dataset> parse(List<String> data) {
+        return null;
+    }
+
+    @Override
+    public void precompute() {
+    }
+}
