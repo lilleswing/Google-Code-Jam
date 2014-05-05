5df1390e333b869e5461c83cad20b8d0c4ebaf8f
Karl Leswing <karl.leswing@schrodinger.com>
2014-05-05 09:30:43 -0400

    b1

--- java-gcj/src/main/java/lilleswing/gcj/a2014/b1/b/B.java
+++ java-gcj/src/main/java/lilleswing/gcj/a2014/b1/b/B.java
@@ -1,4 +1,4 @@
-package current.b;
+package lilleswing.gcj.a2014.b1.b;
 
 
 import com.google.common.base.Function;
@@ -9,10 +9,22 @@ import java.io.*;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Dumb Brute Force, only solves small solution
+ */
 public class B extends Problem<Case> {
     @Override
     public String solve(Case aCase) {
-        return null;
+        int count = 0;
+        for (int ta = 0; ta < aCase.getA(); ta++) {
+            for (int tb = 0; tb < aCase.getB(); tb++) {
+                int tc = ta & tb;
+                if (tc < aCase.getK()) {
+                    count++;
+                }
+            }
+        }
+        return String.valueOf(count);
     }
 
     @Override
@@ -21,6 +33,8 @@ public class B extends Problem<Case> {
         final List<Case> cases = Lists.newArrayList();
         long num_cases = sc.nextLong();
         for (int case_num = 0; case_num < num_cases; case_num++) {
+            Case aCase = new Case(sc.nextInt(), sc.nextInt(), sc.nextInt());
+            cases.add(aCase);
         }
         return cases;
     }
@@ -36,8 +50,23 @@ public class B extends Problem<Case> {
 }
 
 class Case {
-    public Case() {
+    private int a, b, k;
+    public Case(int a, int b, int k) {
+        this.a = a;
+        this.b = b;
+        this.k = k;
+    }
 
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getK() {
+        return k;
     }
 }
 
