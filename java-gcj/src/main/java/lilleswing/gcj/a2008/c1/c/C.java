3c3a931eb7069d7fc140655b6ebcb269f7625b44
Karl Leswing <karl.leswing@schrodinger.com>
2014-08-15 08:34:18 -0400

    still need to clean up the DP on C

--- java-gcj/src/main/java/lilleswing/gcj/a2008/c1/c/C.java
+++ java-gcj/src/main/java/lilleswing/gcj/a2008/c1/c/C.java
@@ -1,4 +1,44 @@
 package lilleswing.gcj.a2008.c1.c;
 
-public class C {
+import com.google.common.collect.Lists;
+import lilleswing.gcj.util.Problem;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class C extends Problem<Case> {
+    @Override
+    public String solve(Case aCase) {
+        return String.valueOf(aCase.getSequences());
+    }
+
+    @Override
+    public List<Case> parse(String data) {
+        final Scanner sc = new Scanner(data);
+        final List<Case> cases = Lists.newArrayList();
+        long num_cases = sc.nextLong();
+        for (int case_num = 0; case_num < num_cases; case_num++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            int Z = sc.nextInt();
+            final List<Long> A = Lists.newArrayList();
+            for(int i = 0; i < m; i++) {
+                A.add(sc.nextLong());
+            }
+            final Case aCase = new Case(n,m,X,Y,Z,A);
+            cases.add(aCase);
+        }
+        return cases;
+    }
+
+    @Override
+    public void preCompute() {
+    }
+
+    public static void main(String[] args) {
+        C c = new C();
+        c.main(args[0]);
+    }
 }
--- java-gcj/src/main/java/lilleswing/gcj/a2008/c1/c/Case.java
+++ java-gcj/src/main/java/lilleswing/gcj/a2008/c1/c/Case.java
@@ -1,4 +1,75 @@
 package lilleswing.gcj.a2008.c1.c;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.util.*;
+
 public class Case {
+    final int MOD = 1000000007;
+    final TreeMap<Integer, Integer> map = Maps.newTreeMap();
+    final List<Integer> limits = Lists.newArrayList();
+    public Case(long n, int m, long x, long y, long z, List<Long> a) {
+        final List<Long> tLimits = Lists.newArrayList();
+        for(int i = 0; i < n; i++) {
+            tLimits.add(a.get(i % m));
+            a.set(i % m, ((x * a.get(i % m)) + (y * (i + 1))) % z);
+        }
+        final List<Long> tLimitValues = Lists.newArrayList(Sets.newHashSet(tLimits));
+        Collections.sort(tLimitValues);
+        final Map<Long, Integer> indexMap = Maps.newHashMap();
+        for(int i = 0; i < tLimitValues.size(); i++) {
+            indexMap.put(tLimitValues.get(i), i);
+        }
+        for (final Long tLimitValue: tLimitValues) {
+            limits.add(indexMap.get(tLimitValue));
+        }
+    }
+
+    public int getSequences() {
+        for(Integer limit: limits) {
+            System.out.println(map);
+            final int toAdd = getToAdd(limit);
+            final int originalValue = getOriginalValue(limit);
+            final int unitySequence = 1;
+            int total = modAdd(unitySequence, modAdd(toAdd, originalValue));
+            map.put(limit, total);
+        }
+        System.out.println(map);
+        int sequences = 0;
+        for (final Integer value: map.values()) {
+            sequences = modAdd(sequences, value);
+        }
+        return sequences;
+    }
+
+    private int getToAdd(Integer limit) {
+        final Iterator<Integer> iterator = map.keySet().iterator();
+        if(!iterator.hasNext()) {
+            return 0;
+        }
+        int value = iterator.next();
+        int toAdd = 0;
+        while (value < limit) {
+            toAdd = modAdd(toAdd, map.get(value));
+            if(!iterator.hasNext()) {
+                return toAdd;
+            }
+            value = iterator.next();
+        }
+        return toAdd;
+    }
+
+    private int getOriginalValue(Integer limit) {
+        if(map.containsKey(limit)) {
+            return map.get(limit);
+        }
+        return 0;
+    }
+
+    public int modAdd(final int a, final int b) {
+        return (a + b) % MOD;
+    }
+
 }
